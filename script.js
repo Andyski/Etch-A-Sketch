f968457ce8761f4a22dfef5d2e201badf4fecd32
Andrew Mischefski <47274079+Andyski@users.noreply.github.com>
2020-06-06 18:15:01 +0100

    Add files via upload
    Added in additional instructions for size button

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
 const container = document.querySelector('#container');
 
-    function createGrid(rows, cols) {
-        container.style.setProperty('--grid-rows', rows);
-        container.style.setProperty('--grid-columns', cols);
+function createGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', cols);
 
-        for(i = 0; i <=(rows * cols); i++) {
-            const gridBox = document.createElement('div');
-            container.appendChild(gridBox).classList.add('gridBox');
-        }
+    for(i = 0; i <=(rows * cols); i++) {
+        const gridBox = document.createElement('div');
+        container.appendChild(gridBox).classList.add('gridBox');
     }
+}
 
-    createGrid(16,16);
+createGrid(16,16);
 
-    function changeColour(e) {
-            e.target.style.backgroundColor = 'black';
-        }
+function changeColour(e) {
+    e.target.style.backgroundColor = 'black';
+}
 
-    const boxes = document.querySelectorAll('.gridBox');
-    boxes.forEach(gridBox => gridBox.addEventListener('mouseover', changeColour));
+const boxes = document.querySelectorAll('.gridBox');
+boxes.forEach(gridBox => gridBox.addEventListener('mouseover', changeColour));
 
-    var size = document.getElementById("gridSize");
-    size.addEventListener("keydown", function (e) {
-        if (e.keyCode === 13) {
+var size = document.getElementById("gridSize");
+size.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
         changeSize(e);
-        }
-    });
-
-    function changeSize() {
-        size = document.getElementById('gridSize').value;
-        if (size == "") { 
-            size = 16;
-            const grids = document.getElementsByClassName('gridBox');
-            while (grids.length > 0) grids[0].remove();
-            createGrid(size, size);
-            const box = document.querySelectorAll('.gridBox');
-            box.forEach(gridBox => gridBox.addEventListener('mouseover', changeColour));
-            var size = document.getElementById("gridSize");
-            size.addEventListener("keydown", function (e) {
-                if (e.keyCode === 13) {
-                changeSize(e);
-                }
-            });
-        } 
-        else if (size > 100) { 
-           alert("Your grid size is too large, try 100 or below")
-        } 
-        else {
-            const grids = document.getElementsByClassName('gridBox');
-            while (grids.length > 0) grids[0].remove();
-            createGrid(size, size);
-            const box = document.querySelectorAll('.gridBox');
-            box.forEach(gridBox => gridBox.addEventListener('mouseover', changeColour));
-            var size = document.getElementById("gridSize");
-            size.addEventListener("keydown", function (e) {
-                if (e.keyCode === 13) {
-                changeSize(e);
-                }
-            });
-        }
     }
+});
+
+var help = document.getElementById("helpIcon");
+help.addEventListener("mouseover", makeInstructions);
+help.addEventListener("mouseout", removeInstructions);
+
+function makeInstructions() {
+    //code to create the instructions once the mouse is targeted on the help icon
+    let instructions = document.querySelector('p1');
+    instructions.textContent = "You can change grid size by clicking the #Size button, then typing a number between 1-100, then press enter or click reset.";
+}
+
+function removeInstructions() {
+    //code to remove the instrucitons once mouse is not targeted on the help icon
+    let instructions = document.querySelector('p1');
+    instructions.textContent = "";
+}
+
+function changeSize() {
+    size = document.getElementById('gridSize').value;
+    if (size == "") { 
+        size = 16;
+        const grids = document.getElementsByClassName('gridBox');
+        while (grids.length > 0) grids[0].remove();
+        createGrid(size, size);
+        const box = document.querySelectorAll('.gridBox');
+        box.forEach(gridBox => gridBox.addEventListener('mouseover', changeColour));
+        var size = document.getElementById("gridSize");
+        size.addEventListener("keydown", function (e) {
+            if (e.keyCode === 13) {
+            changeSize(e);
+            }
+        });
+    } 
+    else if (size > 100) { 
+        alert("Your grid size is too large, try 100 or below")
+    } 
+    else {
+        const grids = document.getElementsByClassName('gridBox');
+        while (grids.length > 0) grids[0].remove();
+        createGrid(size, size);
+        const box = document.querySelectorAll('.gridBox');
+        box.forEach(gridBox => gridBox.addEventListener('mouseover', changeColour));
+        var size = document.getElementById("gridSize");
+        size.addEventListener("keydown", function (e) {
+            if (e.keyCode === 13) {
+            changeSize(e);
+            }
+        });
+    }
+}
